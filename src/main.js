ca67540905d2106e5832af0e2258b4dd6f19b5b7
Duc Dao <huuduc.uneti@gmail.com>
2016-12-03 00:48:40 +0700

    Introducing JSX

--- src/main.js
+++ src/main.js
@@ -2,14 +2,37 @@ console.log('Hello World!');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
- 
-document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('READY');
+});
+
+ReactDOM.render(
     React.createElement(Counter),
     document.getElementById('hello')
-  );
-  ReactDOM.render(
+);
+
+ReactDOM.render(
     <h1>Main</h1>,
     document.getElementById('main')
-  );
-});
+);
+
+const User = {
+    firstname: 'John',
+    lastname: 'Smith'
+};
+
+function formatName(user) {
+    return user.firstname + ' ' + user.lastname;
+}
+
+const element = (
+    <h1>
+        Hello, {formatName(User)}
+    </h1>
+);
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
