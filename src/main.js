4ea197e48d58e3ba898a7c03ffb76696183e0e68
Duc Dao <huuduc.uneti@gmail.com>
2016-12-03 19:20:58 +0700

    Update

--- src/main.js
+++ src/main.js
@@ -6,17 +6,19 @@ import Counter from './Counter';
 document.addEventListener('DOMContentLoaded', function () {
     console.log('READY');
 });
-
+// Intro
 ReactDOM.render(
     React.createElement(Counter),
     document.getElementById('hello')
 );
 
+// Hello
 ReactDOM.render(
     <h1>Main</h1>,
     document.getElementById('main')
 );
 
+// JSX
 const User = {
     firstname: 'John',
     lastname: 'Smith'
@@ -36,3 +38,19 @@ ReactDOM.render(
     element,
     document.getElementById('root')
 );
+
+// Elements
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById('time')
+    );
+}
+
+setInterval(tick, 1000);
